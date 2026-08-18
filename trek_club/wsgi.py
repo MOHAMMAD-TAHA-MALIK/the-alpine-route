8b8e6069270841cb8fc065b1527835eabff15495
MOHAMMAD TAHA MALIK <mmaliktaha01@gmail.com>
2026-08-18 22:35:06 +0530

    requirementss

--- trek_club/wsgi.py
+++ trek_club/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for trek_club project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trek_club.settings')
 
 application = get_wsgi_application()
+
+# Run database migrations automatically on server startup
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Startup migration failed: {e}")
